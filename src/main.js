eef280d2e44498a1018d475a6b4cf29c20abc714
fozeryPC <fozery@yandex.ru>
2024-04-07 13:43:40 +0300

    feat: delete auto-animate plugin from main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import '../node_modules/@splidejs/splide/dist/css/splide.min.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
 import { mask } from 'vue-the-mask';
 
 import App from './App.vue';
@@ -13,7 +12,6 @@ const app = createApp(App);
 
 app.directive('mask', mask);
 
-app.use(autoAnimatePlugin);
 app.use(createPinia());
 app.use(router);
 app.mount('#app');
