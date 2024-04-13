36a559067bab38e32d8eb8057cbd347cfde6bc11
fozeryPC <fozery@yandex.ru>
2024-04-13 18:07:03 +0300

    feat: add isAuth ref to rootStore

--- src/stores/rootStore.js
+++ src/stores/rootStore.js
@@ -3,8 +3,10 @@ import { ref } from 'vue';
 
 export const useRootStore = defineStore('rootStore', () => {
     const isMobile = ref(false);
+    const isAuth = ref(false);
 
     return {
         isMobile,
+        isAuth,
     };
 });
