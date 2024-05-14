384d010f80dc6cbbe26b719484353d9e8d340fde
fozery <fozery@yandex.ru>
2024-05-14 19:55:44 +0300

    fix: change styles for ProductListItemSkeleton

--- src/components/Skeletons/ProductListItemSkeleton.vue
+++ src/components/Skeletons/ProductListItemSkeleton.vue
@@ -1,9 +1,9 @@
 <template>
     <div
-        class="flex pb-3 sm:p-0 gap-2 sm:flex-col border-b last:border-0 sm:border-0 cursor-pointer"
+        class="flex pb-3 sm:p-0 gap-2 sm:flex-col border-b last:border-0 sm:border-0 "
     >
         <div class="skeleton border-2 rounded-full w-28 h-28 sm:w-52 sm:h-52 sm:mx-auto" />
-        <div class="flex flex-col gap-3 justify-between flex-1">
+        <div class="flex flex-col gap-4 justify-between flex-1 mt-3">
             <div>
                 <div class="skeleton rounded-lg max-w-36 sm:w-40 h-6 mb-1"></div>
                 <div class="skeleton max-w-80 sm:max-w-full rounded-xl h-10"></div>
