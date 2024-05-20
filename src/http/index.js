9752159e0531352182e5618f912b205de6b4707f
fozery <fozery@yandex.ru>
2024-05-20 22:12:34 +0300

    feat: add new axios interceptor for admin auth

--- src/http/index.js
+++ src/http/index.js
@@ -5,26 +5,37 @@ const $host = axios.create({
 });
 
 const $authHost = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL
+    baseURL: import.meta.env.VITE_APP_API_URL,
+});
+
+const $adminHost = axios.create({
+    baseURL: import.meta.env.VITE_APP_API_URL,
 });
 
 const authInterceptor = config => {
-    config.headers.authorization = `Bearer ${localStorage.getItem('jwt')}`;
+    config.headers.Authorization = `Bearer ${localStorage.getItem('jwt')}`;
     return config;
-}
+};
 
-const authErrorResponseInterceptor = error =>
-{
-    if(error.response.status === 401) {
+const authErrorResponseInterceptor = error => {
+    if (error.response.status === 401) {
         localStorage.removeItem('jwt');
     }
     return Promise.reject(error);
-}
+};
+
+const adminAuthInterceptor = config => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('jwt-admin')}`;
+    return config;
+};
 
 $authHost.interceptors.request.use(authInterceptor);
-$authHost.interceptors.response.use(response => response, authErrorResponseInterceptor);
+$authHost.interceptors.response.use(config => config, authErrorResponseInterceptor);
+
+$adminHost.interceptors.request.use(adminAuthInterceptor);
 
 export {
     $host,
-    $authHost
-}
+    $authHost,
+    $adminHost,
+};
