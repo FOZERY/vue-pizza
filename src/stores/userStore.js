46d40299b9a390598c844d9d35249617834bcd72
fozery <fozery@yandex.ru>
2024-05-20 22:12:04 +0300

    feat: add functions userAPI function to userStore

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,5 +1,6 @@
-import axios from 'axios';
 import { defineStore } from 'pinia';
+import { $authHost, $host } from '@/http/index.js';
+import { jwtDecode } from 'jwt-decode';
 
 export const useUserStore = defineStore('userStore', {
     state: () => ({
@@ -10,8 +11,34 @@ export const useUserStore = defineStore('userStore', {
     getters: {},
 
     actions: {
-        async hasToken() {
-            return localStorage.getItem('jwt');
-        }
+        async sendOtp(phone) {
+            const response = await $host.post('/customer/sendinformationcode.php', {
+                phone: phone,
+            }, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
+            return response;
+        },
+        async login(phone, code, otp_id) {
+            const { data } = await $host.post('/customer/login.php', {
+                phone: phone,
+                code: code,
+                otp_id: otp_id,
+                role: 'user',
+            }, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
+            localStorage.setItem('jwt', data.jwt);
+            const jwtDecoded = jwtDecode(data.jwt);
+            return jwtDecoded.data;
+        },
+
+        async   check() {
+            const { data } = await $authHost.get('/customer/auth.php');
+            localStorage.setItem('jwt', data.jwt);
+            const jwtDecoded = jwtDecode(data.jwt);
+            return jwtDecoded.data;
+        },
+
+        async fetchUser(id) {
+            const { data } = await $authHost.get('/customer/readOne.php', { params: { id: id } });
+            this.user = data;
+        },
     },
 });
