9884ea28de219acad551e97114af71d4d7815bf0
fozery <fozery@yandex.ru>
2024-05-20 22:14:49 +0300

    feat: change updateCustomer function in customersStore

--- src/stores/customersStore.js
+++ src/stores/customersStore.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { defineStore } from 'pinia';
+import { $adminHost, $authHost } from '@/http/index.js';
 
 const API_URL = import.meta.env.VITE_APP_API_URL;
 
@@ -8,12 +9,10 @@ export const useCustomersStore = defineStore('customersStore', {
         customers: [],
         sortBy: '',
         searchBy: '',
-        isLoading: false
+        isLoading: false,
     }),
 
-    getters: {
-       
-    },
+    getters: {},
 
     actions: {
         async fetchCustomers() {
@@ -21,49 +20,57 @@ export const useCustomersStore = defineStore('customersStore', {
                 this.isLoading = true;
                 this.customers = [];
                 const params = {};
-                if(this.sortBy) {
+                if (this.sortBy) {
                     params.sortBy = this.sortBy;
                 } else {
                     this.sortBy = 'id';
                 }
 
-                if(this.searchBy) {
-                    params.searchBy = this.searchBy
+                if (this.searchBy) {
+                    params.searchBy = this.searchBy;
                 }
 
-                const { data } = await axios.get(`${API_URL}/customer/read.php`, {params});
-                this.customers = data; 
+                const { data } = await axios.get(`${API_URL}/customer/read.php`, { params });
+                this.customers = data;
                 this.isLoading = false;
-            } catch(err) {
+            } catch (err) {
                 console.log(err);
             }
         },
+
         async addCustomer() {
 
         },
         async updateCustomer(customerData) {
             try {
                 this.isLoading = true;
-                const fd = new FormData();
-                for(let key in customerData) {
-                    fd.append(key, customerData[key]);
-                }
-                const response = await axios.post(`${API_URL}/customer/update.php`, fd);
+                const response = await $authHost.patch(`${API_URL}/customer/update.php`, customerData);
                 this.isLoading = false;
-            } catch(err) {
-                alert(err);
+            } catch (err) {
+                console.log(err);
+                throw err;
+            }
+        },
+        async updateCustomerAsAdmin(customerData) {
+            try {
+                this.isLoading = true;
+                const response = await $adminHost.patch(`${API_URL}/customer/update.php`, customerData);
+                this.isLoading = false;
+            } catch (err) {
+                console.log(err);
+                throw err;
             }
         },
         async deleteCustomer(customerId) {
-             try {
-               this.isLoading = true;
-               const fd = new FormData();
-               fd.append('id', customerId);
-               await axios.post(`${API_URL}/customer/delete.php`, fd);
+            try {
+                this.isLoading = true;
+                const fd = new FormData();
+                fd.append('id', customerId);
+                await axios.post(`${API_URL}/customer/delete.php`, fd);
 
-            } catch(err) {
+            } catch (err) {
                 alert(err);
             }
-        }
-    }
+        },
+    },
 });
